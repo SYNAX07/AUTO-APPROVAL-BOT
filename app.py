f8b412cb0a5f1b17b5ae85c82eb1721a9d5b1ade
Ashwani <162049637+SYNAX07@users.noreply.github.com>
2024-04-17 22:43:29 +0530

    𝙰𝚄𝚃𝙾 𝙰𝙿𝙿𝚁𝙾𝚅𝙰𝙻 𝙱𝙾𝚃 🥀𝙼𝙰𝙳𝙴 𝙱𝚈 𝚂𝚈𝙽𝙰𝚇🥀

--- app.py
+++ app.py
@@ -3,12 +3,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'TechVJ'
+    return 'Quantabots'
 
 
 if __name__ == "__main__":
     app.run()
 
-# Don't Remove Credit @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot @Tech_VJ
-# Ask Doubt on telegram @KingVJ01
+# 𝙳𝚘𝚗'𝚝 𝚁𝚎𝚖𝚘𝚟𝚎 𝙲𝚛𝚎𝚍𝚒𝚝 @ASHWANI10
+# 𝚂𝚞𝚋𝚜𝚌𝚛𝚒𝚋𝚎 Telegram 𝙲𝚑𝚊𝚗𝚗𝚎𝚕 𝙵𝚘𝚛 𝙰𝚖𝚊𝚣𝚒𝚗𝚐 𝙱𝚘𝚝 @Quantabots
+# 𝙰𝚜𝚔 𝙳𝚘𝚞𝚋𝚝 𝚘𝚗 𝚝𝚎𝚕𝚎𝚐𝚛𝚊𝚖 @OGsnx
